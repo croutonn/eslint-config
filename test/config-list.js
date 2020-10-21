c2770ee75aff35e48c81fd35a8d4235cca7f2e0f
croutonn <croutonna@gmail.com>
2020-10-22 00:05:07 +0900

    Refactor tests

---
+++ test/config-list.js
@@ -0,0 +1,14 @@
+module.exports = [
+  'index',
+  'javascript',
+  'javascript-test',
+  'javascript-loose',
+  'javascript-react',
+  'javascript-react-loose',
+  'typescript',
+  'typescript-test',
+  'typescript-declare',
+  'typescript-loose',
+  'typescript-react',
+  'typescript-react-loose',
+]
--- test/requires.js
+++ test/requires.js
@@ -1,51 +1,15 @@
-/* eslint strict: 0, global-require: 0 */
+/* eslint strict: 0, global-require: 0, import/no-dynamic-require: 0 */
 
 'use strict'
 
 const test = require('tape')
 
+const configs = require('./config-list')
+
 test('all entry points parse', (t) => {
-  t.doesNotThrow(() => require('..'), 'index does not throw')
-
-  t.doesNotThrow(() => require('../javascript'), 'javascript does not throw')
-  t.doesNotThrow(
-    () => require('../javascript-test'),
-    'javascript-test does not throw'
-  )
-  t.doesNotThrow(
-    () => require('../javascript-loose'),
-    'javascript-loose does not throw'
-  )
-  t.doesNotThrow(
-    () => require('../javascript-react'),
-    'javascript-react does not throw'
-  )
-  t.doesNotThrow(
-    () => require('../javascript-react-loose'),
-    'javascript-react-loose does not throw'
-  )
-
-  t.doesNotThrow(() => require('../typescript'), 'typescript does not throw')
-  t.doesNotThrow(
-    () => require('../typescript-test'),
-    'typescript-test does not throw'
-  )
-  t.doesNotThrow(
-    () => require('../typescript-declare'),
-    'typescript-declare does not throw'
-  )
-  t.doesNotThrow(
-    () => require('../typescript-loose'),
-    'typescript-loose does not throw'
-  )
-  t.doesNotThrow(
-    () => require('../typescript-react'),
-    'typescript-react does not throw'
-  )
-  t.doesNotThrow(
-    () => require('../typescript-react-loose'),
-    'typescript-react-loose does not throw'
-  )
+  configs.forEach(config => {
+    t.doesNotThrow(() => require(`../${config}`), `${config} does not throw`)
+  })
 
   t.end()
 })
