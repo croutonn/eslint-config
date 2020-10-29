3bcf73f3be9d5e6d7a5d1d21ea26a79145660c8d
croutonn <croutonna@gmail.com>
2020-10-30 02:22:15 +0900

    Change react/destructuring-assignment to `never`
    Because in most cases, variable names are assigned inconcretely, making it difficult to know what context a variable comes from.

--- rulesets/react.js
+++ rulesets/react.js
@@ -1,4 +1,5 @@
 module.exports = {
+  'react/destructuring-assignment': ['error', 'never'],
   'react/jsx-uses-react': 'off',
   'react/react-in-jsx-scope': 'off',
   'react/require-default-props': [
