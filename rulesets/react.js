25298ea01f07bd7a2d34203aca3e1cc113edb110
croutonn <croutonna@gmail.com>
2020-11-02 04:16:31 +0900

    Fix critical issue

--- rulesets/react.js
+++ rulesets/react.js
@@ -1,4 +1,4 @@
-const a11y = require('eslint-config-airbnb/rules/react-a11y.js')
+const a11y = require('eslint-config-airbnb/rules/react-a11y.js').rules
 
 module.exports = {
   'react/destructuring-assignment': ['error', 'never'],
@@ -13,6 +13,7 @@ module.exports = {
   ],
   'jsx-a11y/anchor-has-content': 'off',
   'jsx-a11y/control-has-associated-label': 'off',
-  '@croutonn/jsx-a11y-anchor-has-content': a11y["anchor-has-content"],
-  '@croutonn/jsx-a11y-control-has-associated-label': a11y["control-has-associated-label"]
+  '@croutonn/jsx-a11y-anchor-has-content': a11y['anchor-has-content'],
+  '@croutonn/jsx-a11y-control-has-associated-label':
+    a11y['control-has-associated-label'],
 }
