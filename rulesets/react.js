f1944cf4547da8de39186072d25edeb68c7a3061
croutonn <croutonna@gmail.com>
2020-11-02 03:55:56 +0900

    Add two jsx-a11y rules

--- rulesets/react.js
+++ rulesets/react.js
@@ -1,3 +1,5 @@
+const a11y = require('eslint-config-airbnb/rules/react-a11y.js')
+
 module.exports = {
   'react/destructuring-assignment': ['error', 'never'],
   'react/jsx-uses-react': 'off',
@@ -9,4 +11,8 @@ module.exports = {
       ignoreFunctionalComponents: true,
     },
   ],
+  'jsx-a11y/anchor-has-content': 'off',
+  'jsx-a11y/control-has-associated-label': 'off',
+  '@croutonn/jsx-a11y-anchor-has-content': a11y["anchor-has-content"],
+  '@croutonn/jsx-a11y-control-has-associated-label': a11y["control-has-associated-label"]
 }
